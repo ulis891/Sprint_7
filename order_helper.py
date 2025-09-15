148ee835b79ab68d3d64b83cf8e4198b0585c9b9
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-15 22:07:16 +0300

    add order_helper.py

---
+++ order_helper.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+import random
+from faker import Faker
+
+fake = Faker('ru_RU')
+
+
+def get_order_payload(color=None):
+    days_ahead = random.randint(1, 14)
+    delivery_date = (datetime.now() + timedelta(days=days_ahead)).strftime("%Y-%m-%d")
+    firstName = fake.name()
+    lastName = fake.last_name()
+    address = fake.address()
+    metroStation = random.randint(1, 10)
+    phone = fake.phone_number()
+    rentTime = random.randint(1, 7)
+    comment = fake.text(max_nb_chars=10)
+
+    payload = {
+        "firstName": firstName,
+        "lastName": lastName,
+        "address": address,
+        "metroStation": metroStation,
+        "phone": phone,
+        "rentTime": rentTime,
+        "deliveryDate": delivery_date,
+        "comment": comment
+    }
+
+    if color:
+        payload["color"] = color
+
+    return payload
+
+
+
