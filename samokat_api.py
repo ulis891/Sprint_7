79b2bb30d37a28363226d41a8095a2079c6d84b3
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-16 22:56:14 +0300

    fix samokat_api.py

--- samokat_api.py
+++ samokat_api.py
@@ -6,16 +6,22 @@ from data import Data as D
 class SamokatApi:
     @allure.step("Удаление курьера")
     def delete_courier(self, **kwargs):
-        response_id = requests.post(D.BASE_URL + D.LOGIN_COURIER, **kwargs)
-        courier_id = response_id.json()
-        response = requests.delete(D.BASE_URL + D.DELETE_COURIER, data=courier_id)
+        if kwargs:
+            if "id" not in kwargs:
+                response_id = self.login_courier(**kwargs)
+                if response_id.status_code == 200:
+                    courier_id = str(response_id.json()["id"])
+                    response = requests.delete(D.BASE_URL + D.DELETE_COURIER + courier_id)
+                    return response
+            courier_id = kwargs["id"]
+            response = requests.delete(D.BASE_URL + D.DELETE_COURIER + courier_id)
+            return response
+        response = requests.delete(D.BASE_URL + D.DELETE_COURIER)
         return response
 
     @allure.step(f"Создание курьера")
-    def create_courier(self, *args, **kwargs):
+    def create_courier(self, **kwargs):
         response = requests.post(D.BASE_URL + D.CREATE_COURIER, **kwargs)
-        if response.status_code == 201:
-            self.delete_courier(**kwargs)
         return response
 
     @allure.step("Логин курьера")
@@ -26,9 +32,16 @@ class SamokatApi:
     @allure.step("Создание заказа")
     def create_order(self, **kwargs):
         response = requests.post(D.BASE_URL + D.CREATE_ORDER, **kwargs)
+        if response.status_code == 201:
+            self.cancel_order(data=response.json())
         return response
 
     @allure.step("Получение списка заказов")
     def get_orders(self):
         response = requests.get(D.BASE_URL + D.CREATE_ORDER)
+        return response
+
+    @allure.step("Отмена заказа")
+    def cancel_order(self, **kwargs):
+        response = requests.put(D.BASE_URL + D.CANCEL_ORDER, **kwargs)
         return response
