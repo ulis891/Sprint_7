107b04e66c8a8b251db0757e96814d0523c8505a
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-16 22:53:03 +0300

    fix conftest.py

--- conftest.py
+++ conftest.py
@@ -1,13 +1,14 @@
 import pytest
 import string
 import random
+import requests
 from samokat_api import SamokatApi as API
-
 
 
 @pytest.fixture
 def samokat_api():
     return API()
+
 
 @pytest.fixture
 def create_login_password_firstname():
@@ -38,11 +39,21 @@ def create_courier(samokat_api, create_login_password_firstname):
         login_pass.append(create_login_password_firstname["login"])
         login_pass.append(create_login_password_firstname["password"])
         login_pass.append(create_login_password_firstname["firstName"])
+    return login_pass
 
-    yield login_pass
-    payload = {
-        "login": create_login_password_firstname["login"],
-        "password": create_login_password_firstname["password"]
-    }
-    API.delete_courier(samokat_api, data=payload)
+@pytest.fixture
+def create_and_delete_courier(samokat_api, create_login_password_firstname, request):
+    response = samokat_api.create_courier(data=create_login_password_firstname)
+    login_pass = []
+    if response.status_code == 201:
+        login_pass.append(create_login_password_firstname["login"])
+        login_pass.append(create_login_password_firstname["password"])
+        login_pass.append(create_login_password_firstname["firstName"])
 
+        def fin():
+            delete_response = samokat_api.delete_courier(data=create_login_password_firstname)
+            assert delete_response.status_code == 200, f"Ошибка удаления: {delete_response.text}"
+
+        request.addfinalizer(fin)
+
+    return login_pass
