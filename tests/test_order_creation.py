0fbd48b51f89a9253238f2614975b2e4c1401f25
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-16 18:30:22 +0300

    fix test_order_creation.py

--- tests/test_order_creation.py
+++ tests/test_order_creation.py
@@ -1,5 +1,4 @@
 import pytest
-import requests
 import allure
 from data import Data
 import order_helper as OH
@@ -9,8 +8,9 @@ class TestOrderCreation:
 
     @allure.title("Тест создания заказа с разными цветами самоката")
     @pytest.mark.parametrize('color', Data.COLOR_VARIANTS)
-    def test_order_creation(self, color):
+    def test_order_creation(self, samokat_api, color):
         payload = OH.get_order_payload(color)
-        response = requests.post(Data.BASE_URL + Data.ORDER_ENDPOINT, json=payload)
+        response = samokat_api.create_order(json=payload)
         assert response.status_code == 201
         assert "track" in response.json()
+
