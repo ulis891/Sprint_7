ddc883338661a35d0d40d6c734d2490776cbe6e4
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-16 18:33:12 +0300

    fix order_helper.py

--- order_helper.py
+++ order_helper.py
@@ -8,13 +8,13 @@ fake = Faker('ru_RU')
 def get_order_payload(color=None):
     days_ahead = random.randint(1, 14)
     delivery_date = (datetime.now() + timedelta(days=days_ahead)).strftime("%Y-%m-%d")
-    firstName = fake.name()
+    firstName = fake.first_name()
     lastName = fake.last_name()
     address = fake.address()
     metroStation = random.randint(1, 10)
     phone = fake.phone_number()
     rentTime = random.randint(1, 7)
-    comment = fake.text(max_nb_chars=10)
+    comment = fake.text(max_nb_chars=20)
 
     payload = {
         "firstName": firstName,
