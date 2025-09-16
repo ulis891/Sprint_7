d43b6c786fb825e29f25cff1e00df1b22b3ddf21
Aleksandr Kostromin <ulis89@yandex.ru>
2025-09-16 22:55:28 +0300

    fix data.py

--- data.py
+++ data.py
@@ -4,8 +4,9 @@ class Data:
     # ENDPOINTS
     LOGIN_COURIER = "/api/v1/courier/login"
     CREATE_COURIER = "/api/v1/courier"
-    DELETE_COURIER = "/api/v1/courier/:id"
+    DELETE_COURIER = "/api/v1/courier/"
     CREATE_ORDER = "/api/v1/orders"
+    CANCEL_ORDER = "/api/v1/orders/cancel"
 
     COLOR_VARIANTS = [
         ["BLACK"],
